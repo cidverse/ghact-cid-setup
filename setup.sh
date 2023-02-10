2704747c82b294973bc3dd1bf7723b76e8f97325
Philipp Heuer <git@philippheuer.me>
2023-02-11 00:42:17 +0100

    chore: use api to find latest release

--- setup.sh
+++ setup.sh
@@ -7,6 +7,14 @@ CID_VERSION=$1
 # installation
 echo "::group::Installation"
 DOWNLOAD_URL=https://github.com/cidverse/cid/releases/download/$CID_VERSION/linux_amd64
+if [ "$CID_VERSION" = "latest" ]; then
+    if [ -z "${GITHUB_TOKEN:-}" ]; then
+        DOWNLOAD_URL=$(curl -s https://api.github.com/repos/cidverse/cid/releases/latest | jq '.assets[] | select(.name|match("^linux_amd64$")) | .browser_download_url' | tr -d '"')
+    else
+        DOWNLOAD_URL=$(curl --header "Authorization: Bearer $GITHUB_TOKEN" -s https://api.github.com/repos/cidverse/cid/releases/latest | jq '.assets[] | select(.name|match("^linux_amd64$")) | .browser_download_url' | tr -d '"')
+    fi
+fi
+
 echo "Downloading from $DOWNLOAD_URL ..."
 mkdir -p $RUNNER_TOOL_CACHE/cid/bin
 export PATH=$RUNNER_TOOL_CACHE/cid/bin:$PATH
