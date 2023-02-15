31e7177a4d98b05a05b4671f70df0ed199bfb9a1
Philipp Heuer <git@philippheuer.me>
2023-02-15 22:47:15 +0100

    feat: don't need to query latest release version

--- setup.sh
+++ setup.sh
@@ -8,11 +8,7 @@ CID_VERSION=$1
 echo "::group::Installation"
 DOWNLOAD_URL=https://github.com/cidverse/cid/releases/download/$CID_VERSION/linux_amd64
 if [ "$CID_VERSION" = "latest" ]; then
-    if [ -z "${GITHUB_TOKEN:-}" ]; then
-        DOWNLOAD_URL=$(curl -s https://api.github.com/repos/cidverse/cid/releases/latest | jq '.assets[] | select(.name|match("^linux_amd64$")) | .browser_download_url' | tr -d '"')
-    else
-        DOWNLOAD_URL=$(curl --header "Authorization: Bearer $GITHUB_TOKEN" -s https://api.github.com/repos/cidverse/cid/releases/latest | jq '.assets[] | select(.name|match("^linux_amd64$")) | .browser_download_url' | tr -d '"')
-    fi
+    DOWNLOAD_URL=https://github.com/cidverse/cid/releases/latest/download/linux_amd64
 fi
 
 echo "Downloading from $DOWNLOAD_URL ..."
