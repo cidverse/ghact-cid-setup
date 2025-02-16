c6dac0517d28bd8871c195fee9a6bd5a5854d5cb
Philipp Heuer <git@philippheuer.de>
2025-02-16 14:20:23 +0100

    feat: catalog update for v0.5.0

--- setup.sh
+++ setup.sh
@@ -2,20 +2,21 @@
 set -euo pipefail
 
 # input
-CID_VERSION=$1
+CID_VERSION="$1"
+CID_DIR="${RUNNER_TOOL_CACHE}/cid-${CID_VERSION}"
 
 # installation
 echo "::group::Installation"
-DOWNLOAD_URL=https://github.com/cidverse/cid/releases/download/$CID_VERSION/linux_amd64
+DOWNLOAD_URL=https://github.com/cidverse/cid/releases/download/${CID_VERSION}/linux_amd64
 if [ "$CID_VERSION" = "latest" ]; then
     DOWNLOAD_URL=https://github.com/cidverse/cid/releases/latest/download/linux_amd64
 fi
 
 echo "Downloading from $DOWNLOAD_URL ..."
-mkdir -p $RUNNER_TOOL_CACHE/cid/bin
-export PATH=$RUNNER_TOOL_CACHE/cid/bin:$PATH
-curl -L -s -o $RUNNER_TOOL_CACHE/cid/bin/cid $DOWNLOAD_URL
-chmod 755 $RUNNER_TOOL_CACHE/cid/bin/cid
+mkdir -p $CID_DIR/bin
+export PATH=$CID_DIR/bin:$PATH
+curl -L -s -o $CID_DIR/bin/cid $DOWNLOAD_URL
+chmod 755 $CID_DIR/bin/cid
 echo "::endgroup::"
 
 # test
@@ -26,6 +27,8 @@ echo "::endgroup::"
 # catalog
 echo "::group::Catalog"
 cid catalog update
+cid catalog list
+cid executables update
 echo "::endgroup::"
 
 # output
@@ -34,7 +37,7 @@ echo "::group::Output"
 echo "Setting: CID_VERSION=$CID_VERSION"
 echo "CID_VERSION=$CID_VERSION" >> $GITHUB_ENV
 
-echo "Adding Path: $RUNNER_TOOL_CACHE/cid/bin"
-echo "$RUNNER_TOOL_CACHE/cid/bin" >> $GITHUB_PATH
+echo "Adding Path: $CID_DIR/bin"
+echo "$CID_DIR/bin" >> $GITHUB_PATH
 
 echo "::endgroup::"
